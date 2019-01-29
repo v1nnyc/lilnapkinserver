aeb21262978d4afdd8ff1d8c1b1f401d6c1a38af
Vincent Cannalla <vcannall@ucsd.edu>
2019-01-28 19:27:54 -0800

    edited php script

--- scripts/phptestt.php
+++ scripts/phptestt.php
@@ -2,9 +2,9 @@
 <html lang="en-US">
 <head>
 	<meta charset="UTF-8">
-	<title>GIT DEPLOYMENT SCRIPT</title>
+	<title>testing php</title>
 </head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<body>
               <?php
                 phpinfo();
               ?>
