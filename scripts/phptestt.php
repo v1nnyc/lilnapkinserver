757e9236e31d3ea45a1445faf177e21cd551268f
Vincent Cannalla <vcannall@ucsd.edu>
2019-01-28 19:25:40 -0800

    lfjkdafklds

--- scripts/phptestt.php
+++ scripts/phptestt.php
@@ -1,3 +1,12 @@
-<?php 
-  phpinfo(); 
-?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>GIT DEPLOYMENT SCRIPT</title>
+</head>
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+              <?php
+                phpinfo();
+              ?>
+</body>
+</html>
